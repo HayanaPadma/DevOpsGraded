048da02823f515a58f93bdc5b7c9b0ccad6bc2b6
Hayana Padma <hayana.padma@hcl.com>
2021-12-12 12:37:06 +0530

    first commit

---
+++ src/main/java/com/demo/DevOpsGradedApplication.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsGradedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsGradedApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
